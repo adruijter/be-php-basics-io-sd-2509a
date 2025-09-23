fa34b9523432d7fabfef1333817ba6697cb2c2bb
adruijter <adruijter@gmail.com>
2025-09-23 11:52:49 +0200

    Lesweek1

--- app/lesweek1.php
+++ app/lesweek1.php
@@ -8,6 +8,8 @@
     <title>PHP Basics</title>
 </head>
 <body>
+    <h2>PHP basics</h2>
+    <!-- Deze tekst zie je niet in de browswer -->
     <?php
         echo "<p>Hallo Wereld!</p>";
 
@@ -20,9 +22,9 @@
         $leeftijd = 58;
         $schoenmaat = 47;
 
-        echo "<p>Mijn voornaam is: $voornaam $tussenvoegsel $achternaam.</p>";
-        echo "<p>Ik woon in de $straatnaam $huisnummer te $woonplaats.</p>";
-        echo "Mijn leeftijd is: $leeftijd en ik heb schoenmaat $schoenmaat.";
+        echo "<p>Mijn naam is: $voornaam $tussenvoegsel $achternaam.</p>";
+        echo "<p>Ik woon in de: $straatnaam $huisnummer te $woonplaats.</p>";
+        echo "<p>Mijn leeftijd is: $leeftijd en ik heb schoenmaat $schoenmaat.</p>";
     ?>    
 </body>
 </html>
